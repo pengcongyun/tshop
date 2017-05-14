6332fa9f6d00e57b6cfc97dc2f9a0cd8dc62eb43
tshop <412027010@qq.com>
2017-05-14 22:09:27 +0800

    初次

---
+++ www.tshop.com/Application/Common/Conf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'URL_MODEL'=>2,
+    'URL_CASE_INSENSITIVE'  =>  true,   // 默认false 表示URL区分大小写 true则表示不区分大小写
+
+    //数据库
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'admin',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  't_',    // 数据库表前缀
+
+    //
+);
