eb2e86bd1355c3c4a1eea71768d54afd733c0aaa
pengcongyun <412027010@qq.com>
2017-08-24 17:24:07 +0800

    各种测试

--- www.tshop.com/Application/Admin/Controller/TestController.class.php
+++ www.tshop.com/Application/Admin/Controller/TestController.class.php
@@ -65,4 +65,84 @@ class TestController extends Controller
         }
         $this->display('formh5');
     }
+    //ajax抽奖
+    public function price(){
+        if(IS_AJAX){
+            $prize_arr = array(
+                '0' => array('id'=>1,'min'=>1,'max'=>29,'prize'=>'一等奖','v'=>1),
+                '1' => array('id'=>2,'min'=>302,'max'=>328,'prize'=>'二等奖','v'=>2),
+                '2' => array('id'=>3,'min'=>242,'max'=>268,'prize'=>'三等奖','v'=>5),
+                '3' => array('id'=>4,'min'=>182,'max'=>208,'prize'=>'四等奖','v'=>7),
+                '4' => array('id'=>5,'min'=>122,'max'=>148,'prize'=>'五等奖','v'=>10),
+                '5' => array('id'=>6,'min'=>62,'max'=>88,'prize'=>'六等奖','v'=>25),
+                '6' => array('id'=>7,'min'=>array(32,92,152,212,272,332),
+                    'max'=>array(58,118,178,238,298,358),'prize'=>'七等奖','v'=>50)
+            );
+//抽奖开始
+            foreach ($prize_arr as $key => $val) {
+                $arr[$val['id']] = $val['v'];
+            }
+            $rid = $this->getRand($arr); //根据概率获取奖项id
+            $res = $prize_arr[$rid-1]; //中奖项
+            $min = $res['min'];
+            $max = $res['max'];
+            if($res['id']==7){ //七等奖
+                $i = mt_rand(0,5);
+                $result['angle'] = mt_rand($min[$i],$max[$i]);
+            }else{
+                $result['angle'] = mt_rand($min,$max); //随机生成一个角度
+            }
+            $result['prize'] = $res['prize'];
+
+            echo json_encode($result);exit;
+        }
+        $this->display('price');
+    }
+    public function getRand($proArr) {
+        $result = '';
+        //概率数组的总概率精度
+        $proSum = array_sum($proArr);
+        //概率数组循环
+        foreach ($proArr as $key => $proCur) {
+            $randNum = mt_rand(1, $proSum);
+            if ($randNum <= $proCur) {
+                $result = $key;
+                break;
+            } else {
+                $proSum -= $proCur;
+            }
+        }
+        unset ($proArr);
+        return $result;
+    }
+    //幻灯片播放
+    public function lightshow(){
+//        $this->display();
+        $this->display('lightshow1');
+    }
+    //js 休眠
+    public function jssleep(){
+        $this->display();
+    }
+    //获取查询，执行的时间
+    public function querytime(){
+        $stime=microtime(true);
+        $data=M('admin')->select();
+        $etime=microtime(true);
+        $total=$etime-$stime;
+        $this->assign('total',$total);
+        $this->display();
+    }
+    //遮罩层
+    public function zhezhao(){
+        $this->display();
+    }
+    //固定在顶部
+    public function topfix(){
+        $this->display();
+    }
+    //前端验证码
+    public function captcha(){
+        $this->display();
+    }
 }
